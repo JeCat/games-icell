143fb8cef6317e605afd414678f188ebdbad0c99
alee <zq@alee2002.com>
2012-08-15 18:23:44 +0800

    some works

--- bin/ui/UI.js
+++ bin/ui/UI.js
@@ -6,5 +6,23 @@ yc.ui.UI = function(){
 	$(window).bind('onAfterAminoAcidChange',null,function(e,pool,type,num){
 		$('.amino-acid-nums-'+type).text( '♪'+pool[type] ) ;
 	}) ;
+	
+	// 合成蛋白质
+	$( "#protein-composite-progress" ).progressbar({
+		value: 59
+	});
+	
+	this.showProteinComposion = function(){
+		
+		$("#dialog-protein-composite")
+			.width(400).height(300)
+			.css({
+				top: ($(window).height()-300)/2
+				, left: ($(window).width()-400)/2
+				, position: 'absolute'
+				, 'z-index': 100
+			})
+			.show() ;
+	}
 }
 yc.ui.UI.ins = new yc.ui.UI ;
