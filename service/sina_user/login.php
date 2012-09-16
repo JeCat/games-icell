dc8e36c9a78c400c35e1d46af51df2191fea58f5
孔源 <anubiskong@gmail.com>
2012-09-16 15:21:46 +0800

    ...

--- service/sina_user/login.php
+++ service/sina_user/login.php
@@ -6,5 +6,5 @@ include_once( 'saetv2.ex.class.php' );
 
 $o = new SaeTOAuthV2( WB_AKEY , WB_SKEY );
 
-$code_url = $o->getAuthorizeURL( "http://icell.jecat.cn/service/sina_user/callback.php" );
+$code_url = $o->getAuthorizeURL( WB_CALLBACK_URL );
 header("Location:" . $code_url);
