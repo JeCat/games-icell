6e286510f2b611cf3881b77b576cf1c0babe4cb6
lexdene <elephant_liu@mail.dlut.edu.cn>
2012-10-30 15:57:55 +0800

    bullet position

--- bin/inner/skill/OutsideShooter.js
+++ bin/inner/skill/OutsideShooter.js
@@ -15,8 +15,14 @@ yc.inner.skill.OutsideShooter = yc.inner.skill.SkillBase.extend({
 		bullet.setInjureRadius( 30 );
 		
 		// from
-		var cell = this.building()._parent._parent;
-		bullet.setFromPosition( cc.p(cell.x,cell.y) );
+		var b = this.building().getParent().getParent() ;
+		var ptf = yc.util.clientToWindow(
+			b,
+			b.x,
+			b.y
+		);
+		var ptr = cc.p(ptf[0],ptf[1]);
+		bullet.setFromPosition( ptr );
 		
 		// target
 		var cvc = this.findClosestVirusCluster(bullet.fromPosition()) ;
