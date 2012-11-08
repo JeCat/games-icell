c1abeccd423ede1bb970c9d8fa408243bcff9c26
kongyuan <anubiskong@gmail.com>
2012-11-08 14:45:06 +0800

    建筑菜单动画

--- bin/ui/BuildingCreateMenu.js
+++ bin/ui/BuildingCreateMenu.js
@@ -286,30 +286,8 @@ yc.ui.BuildingCreateMenu = function(){
 				itemUi.isBuildingBtn =  true;
 				itemUi.building = item;
 				itemUi.hexgon = hexgon;
-				// itemUi.setScale(0.3,0.3);
-				// itemUi.toPosition = cc.p(  position[0], position[1] );
-				// itemUi.toPosition = cc.p( this.uiCenter[0] + position[0], this.uiCenter[1]  + position[1] );
-				// itemUi.setPosition(cc.p( this.uiCenter[0] + position[0], this.uiCenter[1]  + position[1] ));
-				// itemUi.setPosition(cc.p(  position[0],  position[1] ));
-				// itemUi.setContentSize(new cc.Size(10,10));
 				this.ui.addChild( itemUi );
 			}
-
-			var childrenCount = this.ui.getChildrenCount();
-			if(childrenCount === 0 ){
-				return;
-			}
-			var perBuildingRadian = Math.PI * 2 / childrenCount;
-			var children = this.ui.getChildren();
-			var radius = childrenCount * 16 ;
-
-			for(var buildingBtnIndex in children){
-				var x = Math.sin(perBuildingRadian*buildingBtnIndex)*radius;
-				var y = Math.cos(perBuildingRadian*buildingBtnIndex)*radius;
-
-				children[buildingBtnIndex].toPosition = cc.p( x , y );
-				children[buildingBtnIndex].setPosition( cc.p( x , y ) );
-		    }
 
 			var closeBtn = cc.MenuItemImage.create(
 		        "res/btn-no.png",
@@ -322,14 +300,32 @@ yc.ui.BuildingCreateMenu = function(){
 		    closeMenu.setPosition( cc.p( 0 , 0) );
 		    this.ui.addChild(closeMenu);
 
-			// itemUi.setPosition(cc.p( this.uiCenter[0] + position[0], this.uiCenter[1]  + position[1] ));
-		 //    cc.MoveTo.create(2, cc.p(s.width - 40, s.height - 40));
-
 		 	this.onProteinsChanged();
 
 		 	this.ui.setPosition(cc.p( this.uiCenter[0] ,this.uiCenter[1]));
 		 	this.ui.setAnchorPoint( cc.p(0.5,0.5) );
 		 	// this.ui.setRotation(20);
+
+		 	var childrenCount = this.ui.getChildrenCount() - 1;
+			if(childrenCount === 0 ){
+				return;
+			}
+			var perBuildingRadian = Math.PI * 2 / childrenCount;
+			var children = this.ui.getChildren();
+			var radius = childrenCount * 16 ;
+
+		 	for(var buildingBtnIndex in children){
+		 		if(!children[buildingBtnIndex]._rect){
+		 			continue;
+		 		}
+
+		 		var x = Math.sin(perBuildingRadian*buildingBtnIndex)*radius;
+				var y = Math.cos(perBuildingRadian*buildingBtnIndex)*radius;
+
+		 		var moveAct = cc.MoveTo.create(0.1, cc.p( x , y ));
+				children[buildingBtnIndex].runAction(moveAct);
+		    }
+
 		}
 	}
 	
