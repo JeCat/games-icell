1a8ec6a5d4ccc8dfd93eb517d6c7653788c80d56
kongyuan <anubiskong@gmail.com>
2012-12-06 14:07:17 +0800

    登录换图片

--- bin/common.js
+++ bin/common.js
@@ -188,6 +188,8 @@ var g_ResFiles = [
 	, {type:"image", src:"res/mainscene/test1.png"}
 	, {type:"image", src:"res/mainscene/world.png"}
 	, {type:"image", src:"res/mainscene/world1.png"}
+	, {type:"image", src:"res/mainscene/lb.png"}
+	, {type:"image", src:"res/mainscene/cocos.png"}
 	
 	, {type:"image", src:"res/skill.png"}
 	, {type:"image", src:"res/skillbj.png"}
